c68caf77208e9512fcb9f9431d4c21b48e5f73e0
swgtds <swagatadas.sd@outlook.com>
2025-02-14 00:10:11 +0530

    update: modify match fixtures data

--- src/config/channels.ts
+++ src/config/channels.ts
@@ -3,7 +3,7 @@ export interface Channel {
   name: string;
   startTime: string; // 24-hour format "HH:mm"
   endTime: string; // 24-hour format "HH:mm"
-  streamUrl: string;
+  streamUrl: string; // Path to the HTML file for embedding
   match?: {
     team1: string;
     team2: string;
@@ -14,68 +14,68 @@ export interface Channel {
 
 export const channels: Channel[] = [
   {
-    id: "tnt-1",
+    id: "pak-vs-nz",
     name: "Live Match 1",
     startTime: "14:30",
     endTime: "23:00",
-    streamUrl: "https://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/clients/dash/enc/wf8usag51e/out/v1/bd3b0c314fff4bb1ab4693358f3cd2d3/cenc.mpd",
+    streamUrl: "/videos/sky-sports-3.html",
     match: {
       team1: "Pakistan",
       team2: "New Zealand",
       date: "2025-02-19",
-      thumbnail: "https://thesportsprince.com/wp-content/uploads/2024/12/7-2.webp"
-    }
+      thumbnail: "https://thesportsprince.com/wp-content/uploads/2024/12/7-2.webp",
+    },
   },
   {
-    id: "tnt-1",
+    id: "ind-vs-bangladesh",
     name: "Live Match 2",
     startTime: "14:30",
     endTime: "23:00",
-    streamUrl: "https://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/clients/dash/enc/wf8usag51e/out/v1/bd3b0c314fff4bb1ab4693358f3cd2d3/cenc.mpd",
+    streamUrl: "/videos/sky-sports-3.html",
     match: {
       team1: "India",
       team2: "Bangladesh",
       date: "2025-02-20",
-      thumbnail: "https://thesportsprince.com/wp-content/uploads/2024/12/7-2.webp"
-    }
+      thumbnail: "https://thesportsprince.com/wp-content/uploads/2024/12/7-2.webp",
+    },
   },
   {
-    id: "tnt-1",
+    id: "afg-vs-sa",
     name: "Live Match 3",
     startTime: "14:30",
     endTime: "23:00",
-    streamUrl: "https://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/clients/dash/enc/wf8usag51e/out/v1/bd3b0c314fff4bb1ab4693358f3cd2d3/cenc.mpd",
+    streamUrl: "/videos/sky-sports-3.html",
     match: {
       team1: "Afghanistan",
       team2: "South Africa",
       date: "2025-02-21",
-      thumbnail: "https://thesportsprince.com/wp-content/uploads/2024/12/7-2.webp"
-    }
+      thumbnail: "https://thesportsprince.com/wp-content/uploads/2024/12/7-2.webp",
+    },
   },
   {
-    id: "tnt-1",
+    id: "aus-vs-eng",
     name: "Live Match 4",
     startTime: "14:30",
     endTime: "23:00",
-    streamUrl: "https://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/clients/dash/enc/wf8usag51e/out/v1/bd3b0c314fff4bb1ab4693358f3cd2d3/cenc.mpd",
+    streamUrl: "/videos/sky-sports-3.html",
     match: {
       team1: "Australia",
       team2: "England",
       date: "2025-02-22",
-      thumbnail: "https://thesportsprince.com/wp-content/uploads/2024/12/7-2.webp"
-    }
+      thumbnail: "https://thesportsprince.com/wp-content/uploads/2024/12/7-2.webp",
+    },
   },
   {
-    id: "tnt-1",
+    id: "ind-vs-pak",
     name: "Live Match 5",
     startTime: "14:30",
     endTime: "23:00",
-    streamUrl: "https://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/clients/dash/enc/wf8usag51e/out/v1/bd3b0c314fff4bb1ab4693358f3cd2d3/cenc.mpd",
+    streamUrl: "/videos/sky-sports-3.html",
     match: {
       team1: "India",
-      team2: "pakistan",
+      team2: "Pakistan",
       date: "2025-02-23",
-      thumbnail: "https://thesportsprince.com/wp-content/uploads/2024/12/7-2.webp"
-    }
+      thumbnail: "https://thesportsprince.com/wp-content/uploads/2024/12/7-2.webp",
+    },
   },
 ];
