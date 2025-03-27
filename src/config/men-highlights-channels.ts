0a37dfeecab98abda69d442943f14057739ca95d
swgtds <swagatadas.sd@outlook.com>
2025-03-28 00:02:52 +0530

    feat(channels): add new channels for men's match highlights

---
+++ src/config/men-highlights-channels.ts
@@ -0,0 +1,26 @@
+export interface menHighlightChannels {
+    id: string;
+    title: string;
+    tournament: string;
+    date: string; 
+    team1: string;
+    team2: string;
+    url: string; 
+    thumbnail: string; 
+    description: string;
+  }
+  
+  export const menHighlightChannels: menHighlightChannels[] = [
+    {
+      id: "1",
+      title: "Sunrisers Hyderabad vs Lucknow SUpeer Giants",
+      tournament: "IPL 2025",
+      date: "2024-03-27",
+      team1: "SRH",
+      team2: "LSG",
+      url: "https://www.hotstar.com/in/sports/cricket/srh-vs-lsg-highlights/1540040197/video/highlights/watch",
+      thumbnail: "https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/233/1743095350233-i",
+      description: "Match Highlights: LSG won by 5 Wickets!"
+    },
+
+  ];
