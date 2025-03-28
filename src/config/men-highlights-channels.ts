b7cffa6bc57fc80c346674fa33709109518300e1
swgtds <swagatadas.sd@outlook.com>
2025-03-29 00:07:03 +0530

    feat(highlights): add new match highlight

--- src/config/men-highlights-channels.ts
+++ src/config/men-highlights-channels.ts
@@ -20,7 +20,17 @@ export interface menHighlightChannels {
       team2: "LSG",
       url: "https://www.hotstar.com/in/sports/cricket/srh-vs-lsg-highlights/1540040197/video/highlights/watch",
       thumbnail: "https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/233/1743095350233-i",
-      description: "Match Highlights: LSG won by 5 Wickets!"
+      description: "Shardul Thakur's 4/34 curbed Sunrisers Hyderabad to 190/9 before Nicholas Pooran and Mitchell Marsh sealed the chase for Lucknow Super Giants in TATA IPL 2025"
     },
-
+    {
+      id: "2",
+      title: "Chennai Super Kings Vs Royal Challengers Bangalore",
+      tournament: "IPL 2025",
+      date: "2024-03-28",
+      team1: "CSK",
+      team2: "RCB",
+      url: "https://www.hotstar.com/in/sports/cricket/csk-vs-rcb-highlights/1540040200/video/highlights/watch",
+      thumbnail: "/images/csk-vs-rcb-28-mar.png",
+      description: "Royal Challengers Bengaluru ended their 17-year curse at Chepauk as they shone in all departments to defeat Chennai Super Kings in TATA IPL 2025"
+    },
   ];
